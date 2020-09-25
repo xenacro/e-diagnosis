81ae03c6de81d304b2d57facc5f27e71e4f60268
xenacro <kayush054@gmail.com>
2020-09-25 19:53:50 +0530

    Signup fields added

--- webApp/login/views.py
+++ webApp/login/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from login.forms import SignUpForm
 
 
 
@@ -20,11 +21,15 @@ def dashboardView(request):
 
 def registerView(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login_url')
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('dashboard.html')
 
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/register.html', {'form': form})
